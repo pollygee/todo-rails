edbf27380a8cf517a120aa7c340af16a06c743d5
James Dabbs <jamesdabbs@gmail.com>
2015-06-30 11:07:59 -0400

    Send complete request from JS

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -3,6 +3,13 @@ $(document).on("ready", function(){
 
   $("li.task").on("click", function() {
     $(this).toggleClass("complete")
+
+    // $(this).attr("data-task-id")
+    var task_id = $(this).data("task-id")
+
+    $.ajax("/lists/1/tasks/" + task_id + "/complete", {
+      method: "PATCH"
+    })
   })
 
 })
