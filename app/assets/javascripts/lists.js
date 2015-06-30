3a0e053882cf86fed70cbeae9a9de3c2794fc6ed
James Dabbs <jamesdabbs@gmail.com>
2015-06-30 11:18:57 -0400

    Add error messages from JS

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,14 +1,20 @@
 // $(function() {
 $(document).on("ready", function(){
 
+  $(".notice").hide()
+
   $("li.task").on("click", function() {
     $(this).toggleClass("complete")
 
     // $(this).attr("data-task-id")
     var task_id = $(this).data("task-id")
 
+    // Note: don't actually care about list id here
     $.ajax("/lists/1/tasks/" + task_id + "/complete", {
-      method: "PATCH"
+      method: "PATCH",
+      error: function() {
+        $(".notice").show().text("Mistakes were made")
+      }
     })
   })
 
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def complete
+    1/0
     task = Task.find params[:task_id]
     task.toggle_complete!
     render json: { status: :ok }
